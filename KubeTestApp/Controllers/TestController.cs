5e86d19b3ad189b79f5b14b8a848eea464228e8e
MikeJMcGuire <mike@mcguire.mx>
2021-04-21 16:29:05 +1000

    Updates

---
+++ KubeTestApp/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubeTestApp.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController : ControllerBase
+	{
+		private readonly ILogger<TestController> _logger;
+
+		public TestController(ILogger<TestController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		[Route("[action]")]
+		public ActionResult HealthCheck()
+		{
+			return new ObjectResult(string.Format("OK ({0})", DateTime.Now.Second));
+		}
+	}
+}
